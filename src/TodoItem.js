e922f92ac2b592c65f0beb4dadc4e120ebc4f72c
RizwanNiazFolio3 <rizwanniaz@folio3.com>
2021-07-16 18:28:03 +0500

    Changed components from functions to classes. Added state to the App Component

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,12 +1,17 @@
 import React from "react"
 
-function TodoItem(props) {
-    return (
-        <div className="todo-item">
-            <input type="checkbox" defaultChecked = {props.item.completed}/>
-            <p>{props.item.text}</p>
-        </div>
-    )
+
+class TodoItem extends React.Component
+{
+    render() 
+    {
+        return (
+            <div className="todo-item">
+                <input type="checkbox" defaultChecked = {this.props.item.completed}/>
+                <p>{this.props.item.text}</p>
+            </div>
+        )
+    }
 }
 
 export default TodoItem
